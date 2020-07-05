be779899c0360ec7d387aa4832c9ea2b07575f5d
edcel22 <edcel23.edcelcalzoestadola@gmail.com>
2020-07-05 16:54:48 +0800

    added the images of steps

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+user Carbon\Carbon;
 use Illuminate\Database\Seeder;
+user Illuminate\Support\Facades\DB;
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -12,5 +14,48 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
         //
+    	DB::table('categories')->insert([
+    		[
+    			'user_id' => '1',
+    			'name' => 'Laravel',
+    			'slug' => 'laravel',
+    			'is_published' => '1',
+    			'created_at' => Carbon::now(),
+    			'updated_at' => Carbon::now()
+    		],
+    		[
+    			'user_id' => '2',
+    			'name' => 'CodeIgniter',
+    			'slug' => 'CodeIgniter',
+    			'is_published' => '1',
+    			'created_at' => Carbon::now(),
+    			'updated_at' => Carbon::now()
+    		],
+    		[
+    			'user_id' => '3',
+    			'name' => 'Yii',
+    			'slug' => 'Yii',
+    			'is_published' => '1',
+    			'created_at' => Carbon::now(),
+    			'updated_at' => Carbon::now()
+    		],
+    		[
+    			'user_id' => '4',
+    			'name' => 'Sympfony',
+    			'slug' => 'Sympfony',
+    			'is_published' => '1',
+    			'created_at' => Carbon::now(),
+    			'updated_at' => Carbon::now()
+    		],
+    		[
+    			'user_id' => '5',
+    			'name' => 'Python',
+    			'slug' => 'Python',
+    			'is_published' => '1',
+    			'created_at' => Carbon::now(),
+    			'updated_at' => Carbon::now()
+    		],
+    	]);
+
     }
 }
