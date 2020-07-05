aefd4f82999dc2831df1353b51633f4b05234151
edcel22 <edcel23.edcelcalzoestadola@gmail.com>
2020-07-05 16:34:55 +0800

    first commit

---
+++ app/Gallery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gallery extends Model
+{
+    //
+    protected $fillable = ['user_id', 'image_url'];
+
+    public function user() {
+    	return $this->belongsTo(User::class);
+    }
+
+    public function categories() {
+    	return $this->belongsToMany(Category::class, 'category_posts');
+    }
+}
